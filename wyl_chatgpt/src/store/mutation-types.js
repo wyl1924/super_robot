8641875e3b4567ca90f06845e9f118692fbfb43c
王延领 <1714404171@QQ.COM>
2023-05-17 15:37:43 +0800

    前端代码

---
+++ wyl_chatgpt/src/store/mutation-types.js
@@ -0,0 +1,10 @@
+import USERHEADIMGURL from '@/assets/img/w.png'
+import AIHEADIMGURL from '@/assets/img/th.jpg'
+//AI头像地址设置
+export const AI_HEAD_IMG_URL = AIHEADIMGURL
+//用户头像地址设置
+export const USER_HEAD_IMG_URL = USERHEADIMGURL
+//用户名称设置
+export const USER_NAME="经墨"
+
+
---
+++ wyl_chatgpt/src/view/pages/setting.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="setting">
+    <el-container>
+      <el-header>
+        <transition name="el-zoom-in-top">
+          <div v-show="show" class="transition-box">
+            <h3>JUN CHEN MO</h3>
+          </div>
+        </transition>
+      </el-header>
+      <el-main>
+        <transition name="el-zoom-in-top">
+          <div v-show="show" class="transition-box">
+            <span>
+              很感谢大家对我的支持，现已接入OpenAI的Models API、Completions API、Chat API、Audio API、Images API、Files API、Fine-tunes API后续会添加更多有意思的功能进去，希望大家给我的GitHub点个小小的星星，
+              大家如果有什么好的想法可以在GitHub中提出来。
+            </span>
+            <div>
+              <a href="https://space.bilibili.com/326625155?spm_id_from=333.337.0.0">BliBili</a>---<a href="https://github.com/202252197/ChatGPT_JCM">GitHub</a>
+            </div>
+            <div>
+              <h3>愿半生编码，如一生老友</h3>
+            </div>
+          </div>
+        </transition>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      show: false,
+    };
+  },
+  mounted() {
+    this.show = true;
+  },
+};
+</script>
+
+<style>
+  .transition-box {
+    text-align: center;
+    margin-top: 5%;
+    color: #F2F6FC;
+    font-size: 30px;
+  }
+  h1 {
+    color: aliceblue;
+    font-size: 80px;
+  }
+  a {
+    text-decoration: none;
+    color: #67C23A;
+  }
+</style>
